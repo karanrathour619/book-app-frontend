3d9e1109f355c4716ff9b58e10ba0986875f6eec
karanrathour619 <karanrathour619@gmail.com>
2025-01-11 23:47:28 +0530

    ok

--- src/pages/dashboard/editBook/UpdateBook.jsx
+++ src/pages/dashboard/editBook/UpdateBook.jsx
@@ -3,11 +3,12 @@ import InputField from '../addBook/InputField'
 import SelectField from '../addBook/SelectField'
 import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router-dom';
-import { useFetchBookByIdQuery, useUpdateBookMutation } from '../../../redux/features/books/booksApi';
+// import { useFetchBookByIdQuery, useUpdateBookMutation } from '../../../redux/features/books/booksApi';
 import Loading from '../../../components/Loading';
 import Swal from 'sweetalert2';
 import axios from 'axios';
 import getBaseUrl from '../../../utils/baseURL';
+import { useFetchBookByIdQuery, useUpdateBookMutation } from '../../../redux/features/books/booksApi';
 
 const UpdateBook = () => {
   const { id } = useParams();

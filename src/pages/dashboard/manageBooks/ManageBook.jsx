e0b7dd0ea8684eef4bbb77d1f8342353859d245a
karanrathour619 <karanrathour619@gmail.com>
2025-01-11 23:18:20 +0530

    ok

--- src/pages/dashboard/manageBooks/ManageBook.jsx
+++ src/pages/dashboard/manageBooks/ManageBook.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 // import { useDeleteBookMutation, useFetchAllBooksQuery } from '../../../redux/features/books/booksApi';
 import getBaseUrl from '../../../utils/baseURL';
 import axios from 'axios';
-import { useDeleteBookMutation, useFetchAllBooksQuery } from '../../../redux/features/books/booksApi';
+import { useDeleteBookMutation, useFetchAllBooksQuery } from '../../../redux/features/books/booksApi.js';
 
 const ManageBooks = () => {
     const navigate = useNavigate();

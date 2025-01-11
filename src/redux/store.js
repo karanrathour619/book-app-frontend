074e50f948e39a7c7e7c2304b964132dd03718c5
karanrathour619 <karanrathour619@gmail.com>
2025-01-11 23:53:32 +0530

    ok

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from '../redux/features/cart/cartSlice';
-import booksApi from './features/books/booksAPI';
+import booksApi from './features/books/booksApiRedux';
 import ordersApi from './features/orders/ordersApi';
 
 export const store = configureStore({
